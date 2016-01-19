8bd5347d597abe55d8bee18a7f6f5052a4d8c5df
chris-purcell <danzig@rocketmail.com>
2016-01-18 22:00:22 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# A random comic loader specific to xkcd currently
+## Random XKCD
+
+Working on making changes to support more sites, and hopefully improve the page appearance that is generated.
+
---
+++ app.py
@@ -0,0 +1,45 @@
+import os
+from flask import Flask, render_template, url_for, send_from_directory
+import requests
+import random
+from lxml import html
+
+app = Flask(__name__)
+app.config.update(
+    DEBUG=True,
+)
+
+@app.route('/')
+def index():
+
+    return render_template('index.html', data=parse())
+
+def parse():
+    comics_list = []
+
+    #open ten random xkcd comics between 1 & 1446
+
+    for i in range(0,10):
+        source = requests.get("http://xkcd.com/%d" % random.randint(1,1446)).text
+        tree = html.fromstring(source.encode('utf-8'))
+
+        img = tree.xpath('//div[@id="comic"]/img/@src')
+        alt = tree.xpath('//div[@id="comic"]/img/@alt')
+
+        try:
+            img[0]
+        except:
+            img.append(url_for('static',filename='c86.jpg'))
+            alt.append("Error: Image Not Found.")
+
+        comics_list.append({
+            'img':img[0],
+            'alt':alt[0]
+            })
+
+    return comics_list
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
